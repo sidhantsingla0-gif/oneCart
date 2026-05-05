35033ce52d2477767e22f725eec0cdcdeb6a1a4c
Sidhant <sidhantsingla0@gmail.com>
2026-05-05 22:32:23 +0530

    Update Card.jsx

--- frontend/src/component/Card.jsx
+++ frontend/src/component/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { shopDataContext } from '../context/ShopContext'
-import { authDataContext } from '../context/authContext'
+import { authDataContext } from '../context/AuthContext'
 
 function Card({ name, image, id, price }) {
 
