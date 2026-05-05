5b04ebe55427ac3b37fefb04b65e2adc994e1eee
Sidhant <sidhantsingla0@gmail.com>
2026-05-05 22:36:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://onecart-frontend2.onrender.com", "http://localhost:5174"],
     credentials: true
 }))
 app.use(express.static('public'))
